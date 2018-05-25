9e68df167f52f7082a8493bbace37137c4bf4c93
liferlisiqi <lisiqi9310@163.com>
2018-05-25 21:43:18 +0800

    load model done

---
+++ yolo.py
@@ -0,0 +1,287 @@
+import glob
+import csv
+import cv2
+import random
+import numpy as np
+import mxnet as mx
+import time
+from mxnet import autograd
+from mxnet import gluon
+from mxnet import nd
+from mxnet import init
+from mxnet import gpu
+from mxnet.gluon import nn
+from mxnet.gluon import Block, HybridBlock
+from mxnet.gluon.model_zoo import vision
+
+print("all module load")
+
+NUM_CLASS = 43
+BATCH_SIZE = 16
+DATA_SHAPE = 256
+
+train_data = mx.image.ImageIter(
+   batch_size=BATCH_SIZE, label_width = 1,
+   data_shape=(3, DATA_SHAPE, DATA_SHAPE), 
+   path_imgrec='./dataset/dataset.rec',  
+   path_imgidx='./dataset/dataset.idx',  #help shuffle performance
+   shuffle=True)
+
+test_data = mx.image.ImageIter(  
+   batch_size=BATCH_SIZE, label_width = 1,
+   data_shape=(3, DATA_SHAPE, DATA_SHAPE),  
+   path_imgrec='./dataset/dataset.rec',  
+   path_imgidx='./dataset/dataset.idx',  #help shuffle performance
+   shuffle=True)
+
+paths = glob.glob("dataset/scene-jpg/*.jpg")
+labels = nd.zeros((len(paths), NUM_CLASS+1, 5)) -1.
+gts = open("dataset/gt1.txt",'r').read().split('\n')[:-1]
+for gt in gts:
+    line = gt.split(";")
+    idx = int(line[0].split(".")[0])
+    minx = float(line[1])
+    miny = float(line[2])
+    maxx = float(line[3])
+    maxy = float(line[4])
+    label = float(line[5])
+    labels[idx][int(label)] = [label, minx, miny, maxx, maxy]
+
+signname_file = "dataset/signnames.csv"
+with open(signname_file) as f:
+    f.readline() # skip the headers
+    signnames = [row[1] for row in csv.reader(f)]
+
+print("image and label load")
+
+
+def transform_center(xy):
+    """Given x, y prediction after sigmoid(), convert to relative coordinates (0, 1) on image."""
+    b, h, w, n, s = xy.shape
+    offset_y = nd.tile(nd.arange(0, h, repeat=(w * n * 1), ctx=xy.context).reshape((1, h, w, n, 1)), (b, 1, 1, 1, 1))
+    # print(offset_y[0].asnumpy()[:, :, 0, 0])
+    offset_x = nd.tile(nd.arange(0, w, repeat=(n * 1), ctx=xy.context).reshape((1, 1, w, n, 1)), (b, h, 1, 1, 1))
+    # print(offset_x[0].asnumpy()[:, :, 0, 0])
+    x, y = xy.split(num_outputs=2, axis=-1)
+    x = (x + offset_x) / w
+    y = (y + offset_y) / h
+    return x, y
+
+def transform_size(wh, anchors):
+    """Given w, h prediction after exp() and anchor sizes, convert to relative width/height (0, 1) on image"""
+    b, h, w, n, s = wh.shape
+    aw, ah = nd.tile(nd.array(anchors, ctx=wh.context).reshape((1, 1, 1, -1, 2)), (b, h, w, 1, 1)).split(num_outputs=2, axis=-1)
+    w_pred, h_pred = nd.exp(wh).split(num_outputs=2, axis=-1)
+    w_out = w_pred * aw / w
+    h_out = h_pred * ah / h
+    return w_out, h_out
+
+def yolo2_forward(x, num_class, anchor_scales):
+    """Transpose/reshape/organize convolution outputs."""
+    stride = num_class + 5
+    # transpose and reshape, 4th dim is the number of anchors
+    x = x.transpose((0, 2, 3, 1))
+    x = x.reshape((0, 0, 0, -1, stride))
+    # now x is (batch, m, n, stride), stride = num_class + 1(object score) + 4(coordinates)
+    # class probs
+    cls_pred = x.slice_axis(begin=0, end=num_class, axis=-1)
+    # object score
+    score_pred = x.slice_axis(begin=num_class, end=num_class + 1, axis=-1)
+    score = nd.sigmoid(score_pred)
+    # center prediction, in range(0, 1) for each grid
+    xy_pred = x.slice_axis(begin=num_class + 1, end=num_class + 3, axis=-1)
+    xy = nd.sigmoid(xy_pred)
+    # width/height prediction
+    wh = x.slice_axis(begin=num_class + 3, end=num_class + 5, axis=-1)
+    # convert x, y to positions relative to image
+    x, y = transform_center(xy)
+    # convert w, h to width/height relative to image
+    w, h = transform_size(wh, anchor_scales)
+    # cid is the argmax channel
+    cid = nd.argmax(cls_pred, axis=-1, keepdims=True)
+    # convert to corner format boxes
+    half_w = w / 2
+    half_h = h / 2
+    left = nd.clip(x - half_w, 0, 1)
+    top = nd.clip(y - half_h, 0, 1)
+    right = nd.clip(x + half_w, 0, 1)
+    bottom = nd.clip(y + half_h, 0, 1)
+    output = nd.concat(*[cid, score, left, top, right, bottom], dim=4)
+    return output, cls_pred, score, nd.concat(*[xy, wh], dim=4)
+
+def corner2center(boxes, concat=True):
+    """Convert left/top/right/bottom style boxes into x/y/w/h format"""
+    left, top, right, bottom = boxes.split(axis=-1, num_outputs=4)
+    x = (left + right) / 2
+    y = (top + bottom) / 2
+    width = right - left
+    height = bottom - top
+    if concat:
+        last_dim = len(x.shape) - 1
+        return nd.concat(*[x, y, width, height], dim=last_dim)
+    return x, y, width, height
+
+def center2corner(boxes, concat=True):
+    """Convert x/y/w/h style boxes into left/top/right/bottom format"""
+    x, y, w, h = boxes.split(axis=-1, num_outputs=4)
+    w2 = w / 2
+    h2 = h / 2
+    left = x - w2
+    top = y - h2
+    right = x + w2
+    bottom = y + h2
+    if concat:
+        last_dim = len(left.shape) - 1
+        return nd.concat(*[left, top, right, bottom], dim=last_dim)
+    return left, top, right, bottom
+
+def yolo2_target(scores, boxes, labels, anchors, ignore_label=-1, thresh=0.5):
+    """Generate training targets given predictions and labels."""
+    b, h, w, n, _ = scores.shape
+    anchors = np.reshape(np.array(anchors), (-1, 2))
+    #scores = nd.slice_axis(outputs, begin=1, end=2, axis=-1)
+    #boxes = nd.slice_axis(outputs, begin=2, end=6, axis=-1)
+    gt_boxes = nd.slice_axis(labels, begin=1, end=5, axis=-1)
+    target_score = nd.zeros((b, h, w, n, 1), ctx=scores.context)
+    target_id = nd.ones_like(target_score, ctx=scores.context) * ignore_label
+    target_box = nd.zeros((b, h, w, n, 4), ctx=scores.context)
+    sample_weight = nd.zeros((b, h, w, n, 1), ctx=scores.context)
+    for b in range(output.shape[0]):
+        # find the best match for each ground-truth
+        label = labels[b].asnumpy()
+        valid_label = label[np.where(label[:, 0] > -0.5)[0], :]
+        # shuffle because multi gt could possibly match to one anchor, we keep the last match randomly
+        np.random.shuffle(valid_label)
+        for l in valid_label:
+            gx, gy, gw, gh = (l[1] + l[3]) / 2, (l[2] + l[4]) / 2, l[3] - l[1], l[4] - l[2]
+            ind_x = int(gx * w)
+            ind_y = int(gy * h)
+            tx = gx * w - ind_x
+            ty = gy * h - ind_y
+            gw = gw * w
+            gh = gh * h
+            # find the best match using width and height only, assuming centers are identical
+            intersect = np.minimum(anchors[:, 0], gw) * np.minimum(anchors[:, 1], gh)
+            ovps = intersect / (gw * gh + anchors[:, 0] * anchors[:, 1] - intersect)
+            best_match = int(np.argmax(ovps))
+            target_id[b, ind_y, ind_x, best_match, :] = l[0]
+            target_score[b, ind_y, ind_x, best_match, :] = 1.0
+            tw = np.log(gw / (anchors[best_match, 0] + 1e-6))
+            th = np.log(gh / (anchors[best_match, 1] + 1e-6))
+            target_box[b, ind_y, ind_x, best_match, :] = mx.nd.array([tx, ty, tw, th])
+            sample_weight[b, ind_y, ind_x, best_match, :] = 1.0
+            # print('ind_y', ind_y, 'ind_x', ind_x, 'best_match', best_match, 't', tx, ty, tw, th, 'ovp', ovps[best_match], 'gt', gx, gy, gw/w, gh/h, 'anchor', anchors[best_match, 0], anchors[best_match, 1])
+    return target_id, target_score, target_box, sample_weight
+
+class YOLO2Output(HybridBlock):
+    def __init__(self, num_class, anchor_scales, **kwargs):
+        super(YOLO2Output, self).__init__(**kwargs)
+        assert num_class > 0, "number of classes should > 0, given {}".format(num_class)
+        self._num_class = num_class
+        assert isinstance(anchor_scales, (list, tuple)), "list or tuple of anchor scales required"
+        assert len(anchor_scales) > 0, "at least one anchor scale required"
+        for anchor in anchor_scales:
+            assert len(anchor) == 2, "expected each anchor scale to be (width, height), provided {}".format(anchor)
+        self._anchor_scales = anchor_scales
+        out_channels = len(anchor_scales) * (num_class + 1 + 4)
+        with self.name_scope():
+            self.output = nn.Conv2D(out_channels, 1, 1)
+
+    def hybrid_forward(self, F, x, *args):
+        return self.output(x)
+
+print("model done")
+
+sce_loss = gluon.loss.SoftmaxCrossEntropyLoss(from_logits=False)
+l1_loss = gluon.loss.L1Loss()
+from mxnet import metric
+
+class LossRecorder(mx.metric.EvalMetric):
+    """LossRecorder is used to record raw loss so we can observe loss directly
+    """
+    def __init__(self, name):
+        super(LossRecorder, self).__init__(name)
+
+    def update(self, labels, preds=0):
+        """Update metric with pure loss
+        """
+        for loss in labels:
+            if isinstance(loss, mx.nd.NDArray):
+                loss = loss.asnumpy()
+            self.sum_metric += loss.sum()
+            self.num_inst += 1
+
+obj_loss = LossRecorder('objectness_loss')
+cls_loss = LossRecorder('classification_loss')
+box_loss = LossRecorder('box_refine_loss')
+
+positive_weight = 5.0
+negative_weight = 0.1
+class_weight = 1.0
+box_weight = 5.0
+
+pretrained = vision.get_model('resnet18_v1', pretrained=True).features
+net = nn.HybridSequential()
+for i in range(len(pretrained) - 2):
+    net.add(pretrained[i])
+
+# anchor scales, try adjust it yourself
+scales = [[3.3004, 3.59034],[9.84923, 8.23783]]
+
+# use 2 classes, 1 as dummy class, otherwise softmax won't work
+predictor = YOLO2Output(NUM_CLASS, scales)
+predictor.initialize()
+net.add(predictor)
+
+ctx = gpu(0)
+net.collect_params().reset_ctx(ctx)
+trainer = gluon.Trainer(net.collect_params(),
+                        'sgd', {'learning_rate': 1, 'wd': 5e-4})
+
+EPOCHS = 10  # set larger to get better performance
+from_scratch = False  # set to True to train from scratch
+if from_scratch:
+    START_EPOCH = 0
+else:
+    START_EPOCH = 20
+    pretrained = 'params/yolo2_%d.params' % START_EPOCH
+    net.load_params(pretrained, ctx)
+
+print("start train... ", "start epoch:", START_EPOCH, "epochs", EPOCHS)
+
+for epoch in range(START_EPOCH, START_EPOCH+EPOCHS):
+    # reset data iterators and metrics
+    train_data.reset()
+    cls_loss.reset()
+    obj_loss.reset()
+    box_loss.reset()
+    tic = time.time()
+    for i, batch in enumerate(train_data):
+        x = batch.data[0].as_in_context(ctx)
+        batch_label = batch.label[0].asnumpy().astype(int)
+        y = labels[batch_label].as_in_context(ctx)
+        with autograd.record():
+            x = net(x)
+            output, cls_pred, score, xywh = yolo2_forward(x, NUM_CLASS, scales)
+            with autograd.pause():
+                tid, tscore, tbox, sample_weight = yolo2_target(score, xywh, y, scales, thresh=0.5)
+            # losses
+            loss1 = sce_loss(cls_pred, tid, sample_weight * class_weight)
+            score_weight = nd.where(sample_weight > 0,
+                                    nd.ones_like(sample_weight) * positive_weight,
+                                    nd.ones_like(sample_weight) * negative_weight)
+            loss2 = l1_loss(score, tscore, score_weight)
+            loss3 = l1_loss(xywh, tbox, sample_weight * box_weight)
+            loss = loss1 + loss2 + loss3
+        loss.backward()
+        trainer.step(BATCH_SIZE)
+        # update metrics
+        cls_loss.update(loss1)
+        obj_loss.update(loss2)
+        box_loss.update(loss3)
+
+    print('%2d: %s %.5f, %s %.4f, %s %.4f time:%.1f s' % (
+        epoch, *cls_loss.get(), *obj_loss.get(), *box_loss.get(), time.time()-tic))
+    
+net.save_params('params/yolo2_%d.params' % (START_EPOCH + EPOCHS))
+print("save model")
